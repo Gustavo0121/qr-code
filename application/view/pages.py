aa1ebc1896bf8fe68e36c743cf9f0d01219637cb
Gustavo dos Santos Ribeiro <gustavo.ribeiro@presidencia.gov.br>
2024-11-19 16:38:29 -0300

    feat: estrutura inicial da aplicação

---
+++ application/view/pages.py
@@ -0,0 +1,23 @@
+"""Pages."""
+
+import logging
+
+import flet as ft
+
+def not_found_view(e: ft.ControlEvent) -> ft.Control:
+    """Notfount view."""
+    logging.debug(e)
+    return ft.View(
+        route='/notfound',
+        controls=[
+            ft.Column(
+                controls=[
+                    ft.Text(
+                        value='Recurso não encontrado...',
+                        color='red',
+                        weight='bold',
+                    ),
+                ],
+            ),
+        ],
+    )
